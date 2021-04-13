4065a3b18479c0846bc6cd58dc7c70dc6eb73937
Guilherme Guia <guilherme@hapibot.com>
2021-04-13 19:21:25 +0100

    fix: 🐛 add try catch

--- apps/myb/src/app.service.ts
+++ apps/myb/src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import JSONCache from 'redis-json';
 import * as Promise from 'bluebird';
 import * as redis from 'redis';
@@ -10,47 +10,54 @@ export class AppService {
 	}
 
 	async get24HrBinance(): Promise<any> {
-		const client = Promise.promisifyAll(
-			redis.createClient({
-				host: process.env.REDIS_HOST,
-				port: parseInt(process.env.REDIS_PORT),
-			})
-		);
-		await client.auth(process.env.REDIS_PASSWORD);
-
-		const jsonCache = new JSONCache<{
-			symbol: string;
-			priceChange: string;
-			priceChangePercent: string;
-			weightedAvgPrice: string;
-			prevClosePrice: string;
-			lastPrice: string;
-			lastQty: string;
-			bidPrice: string;
-			askPrice: string;
-			openPrice: string;
-			highPrice: string;
-			lowPrice: string;
-			volume: string;
-			quoteVolume: string;
-			openTime: string;
-			closeTime: string;
-			firstId: string; // First tradeId
-			lastId: string; // Last tradeId
-			count: string; // Trade count
-		}>(client);
-
-		var result: any[] = [];
-
-		const res = await client.getAsync('symbol');
-
-		var lstOfSymbols = await res.split(',');
-    await Promise.all(lstOfSymbols.map(async e => {
-      result.push(await jsonCache.get(e));
-    })).then(() => {
-      return result;
-    });
-    
-		return result;  
+		try{
+			const client = Promise.promisifyAll(
+				redis.createClient({
+					host: process.env.REDIS_HOST,
+					port: parseInt(process.env.REDIS_PORT),
+				})
+			);
+			await client.auth(process.env.REDIS_PASSWORD);
+	
+			const jsonCache = new JSONCache<{
+				symbol: string;
+				priceChange: string;
+				priceChangePercent: string;
+				weightedAvgPrice: string;
+				prevClosePrice: string;
+				lastPrice: string;
+				lastQty: string;
+				bidPrice: string;
+				askPrice: string;
+				openPrice: string;
+				highPrice: string;
+				lowPrice: string;
+				volume: string;
+				quoteVolume: string;
+				openTime: string;
+				closeTime: string;
+				firstId: string; // First tradeId
+				lastId: string; // Last tradeId
+				count: string; // Trade count
+			}>(client);
+	
+			var result: any[] = [];
+	
+			const res = await client.getAsync('symbol');
+	
+			var lstOfSymbols = await res.split(',');
+			await Promise.all(
+				lstOfSymbols.map(async (e) => {
+					result.push(await jsonCache.get(e));
+				})
+			).then(() => {
+				return result;
+			});
+	
+			return result;
+		}catch(err){
+			console.log('get24Hr ERROR: ', err);
+			throw new InternalServerErrorException('We are syncing our database with binance, please try again in a few min :)');
+		}
 	}
 }
